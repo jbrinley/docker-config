5927d0dde0f47514dcd470b4ec9297afb3275c77
Jonathan Brinley <jbrinley@flightless.us>
2015-06-16 21:32:50 -0400

    make mysqlsump script path agnostic

--- scripts/mysql-dump-all.sh
+++ scripts/mysql-dump-all.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+# Back up all databases to ../backups/mysql/yyyy-mm-dd
+# Assumes that mysql is running and accessible
+
 USER="root"
 PASSWORD="password"
 HOST="docker"
-OUTPUTDIR="/Users/jbrinley/system/backups/mysql"
+
+DATE=`date +%Y-%m-%d`
+SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+REPODIR=$( dirname "$SCRIPTDIR" )
+OUTPUTDIR="$REPODIR/backups/mysql/$DATE"
+
+echo "Saving backups to $OUTPUTDIR"
 
 mkdir -p $OUTPUTDIR
-#rm "$OUTPUTDIR/*gz" > /dev/null 2>&1
 
 databases=`mysql -u $USER -p$PASSWORD -h $HOST -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 
 for db in $databases; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
         echo "Dumping database: $db"
-        mysqldump -u $USER -p$PASSWORD -h $HOST --databases $db | gzip -c > $OUTPUTDIR/$db.`date +%Y%m%d`.sql.gz
+        mysqldump -u $USER -p$PASSWORD -h $HOST --databases $db | gzip -c > $OUTPUTDIR/$db.sql.gz
     fi
 done
