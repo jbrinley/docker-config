bd8a2a419adb63a900ab4b9b453caa69b510ae58
Jonathan Brinley <jbrinley@flightless.us>
2015-07-03 14:05:22 -0400

    optimize nfs settings

--- scripts/b2d-nfs-setup.sh
+++ scripts/b2d-nfs-setup.sh
@@ -3,6 +3,15 @@
 ############################################################################
 # On OX X host you need to add a line (and file) /etc/exports:
 # /Users -mapall=[youruser]:[yourgroup] [boot2dockerip]
+#
+# e.g.:
+# "/Users" -mapall=501:20 -network 192.168.59 -mask 255.255.255.0
+#
+# Then run this script inside the boot2docker VM
+#
+# $ boot2docker ssh
+# $ /Users/path/to/this/script/b2d-nfs-setup.sh
+#
 # See: https://quip.com/EDYLAAfuup5M (no login needed)
 # See also: https://github.com/boot2docker/boot2docker/issues/587#issuecomment-66935011 
 # See also: http://support.apple.com/en-us/HT202243
@@ -11,4 +20,4 @@
  
 sudo umount /Users
 sudo /usr/local/etc/init.d/nfs-client start
-sudo mount 192.168.59.3:/Users /Users -o rw,async,noatime,rsize=32768,wsize=32768,proto=tcp
+sudo mount 192.168.59.3:/Users /Users -o rw,async,noatime,actimeo=1,intr,rsize=32768,wsize=32768,proto=tcp
